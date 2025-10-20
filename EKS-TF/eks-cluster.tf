0b53d1bce99946e65d7c763fb314ad18e4d14267
jaseflash <84444515+jaseflash@users.noreply.github.com>
2025-10-20 11:42:35 +0100

    Update EKS module configuration and node groups
    updating TF code, modules and versions

--- EKS-TF/eks-cluster.tf
+++ EKS-TF/eks-cluster.tf
@@ -1,43 +1,57 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = local.cluster_name
-  cluster_version = "1.22"
-  subnets         = module.vpc.private_subnets
+  version         = "21.3.2"
+  name    = local.cluster_name
+# version = "~> 19.0" # Or a specific version like "19.14.0"
+  kubernetes_version = "1.32"
+# subnets         = module.vpc.private_subnets
 
-  tags = {
-    Environment = "jase-training"
-    GithubRepo  = "terraform-aws-eks"
-    GithubOrg   = "terraform-aws-modules"
-  }
+# vpc_id = module.vpc.vpc_id
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
-  vpc_id = module.vpc.vpc_id
+  endpoint_public_access  = true
+  endpoint_private_access = true
+# public_access_cidrs     = ["0.0.0.0/0"]
 
-  workers_group_defaults = {
-    root_volume_type = "gp2"
-  }
+  enable_cluster_creator_admin_permissions = true
 
-  worker_groups = [
-    {
-      name                          = "jase-worker-group-1"
+  self_managed_node_groups = {
+    jaseb-group-1 = {
+      name                          = "jaseb-group-1"
       instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 2
+#     additional_userdata           = "echo foo bar"
+
+# Required User Data for self-managed node groups
+      additional_userdata = <<-EOT
+        /etc/eks/bootstrap.sh ${local.cluster_name} --use-max-pods 20 --kubelet-extra-args '--node-labels=Name=${local.cluster_name}-worker'
+      EOT
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      asg_desired_capacity          = 2
+      root_volume_type              = "gp2"
+      iam_instance_profile          = aws_iam_instance_profile.node_profile.name
     },
-    {
-      name                          = "jase-worker-group-2"
+    jaseb-group-2 = {
+      name                          = "jaseb-group-2"
       instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
+#      additional_userdata           = "echo foo bar"
+# Required User Data for self-managed node groups
+      additional_userdata = <<-EOT
+        /etc/eks/bootstrap.sh ${local.cluster_name} --use-max-pods 20 --kubelet-extra-args '--node-labels=Name=${local.cluster_name}-worker'
+      EOT
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
+      root_volume_type              = "gp2"
+      iam_instance_profile          = aws_iam_instance_profile.node_profile.name
     },
-  ]
+  }
 }
 
+
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
